ab0885ec9304523905736cef0d781432b558ddff
ubed-cilok <bidpendikalmatuq@gmail.com>
2026-02-14 14:41:29 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,19 @@
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open('tasmi').then(c=>c.addAll(['/'])))
+const CACHE = "tasmi-pwa-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/app.js",
+  "/manifest.json"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
 });
-self.addEventListener('fetch',e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
 });
