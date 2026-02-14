140d0a4851d28fd002aee7386b97d13fdf413d31
ubed-cilok <bidpendikalmatuq@gmail.com>
2026-02-14 14:37:53 +0700

    Add files via upload
    update lembar tasmi

--- sw.js
+++ sw.js
@@ -1,19 +1,6 @@
-const CACHE = "tasmi-pwa-v1";
-const ASSETS = [
-  "/",
-  "/index.html",
-  "/app.js",
-  "/manifest.json"
-];
-
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
-  );
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open('tasmi').then(c=>c.addAll(['/'])))
 });
-
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+self.addEventListener('fetch',e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))
 });
