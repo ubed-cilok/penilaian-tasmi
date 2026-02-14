f97554cf5d9f1e2a92cb493c53dc23230766ba9b
ubed-cilok <bidpendikalmatuq@gmail.com>
2026-02-14 14:40:01 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
-function exportPDF(){
-  const data = `Nama: ${nama.value}
-Kelas: ${kelas.value}
-Juz: ${juz.value}
-Total: ${total.innerText}
-Akhir: ${akhir.innerText}`;
-
-  const blob = new Blob([data], {type:'text/plain'});
-  const a = document.createElement('a');
-  a.href = URL.createObjectURL(blob);
-  a.download = `Nilai_${nama.value||'santri'}.pdf`;
-  a.click();
-
-  resetForm();
+const KEY='DATA_TASMI';
+function muatTabel(){}
+function simpan(){
+  const h=hitung();
+  const d=[nama.value,kelas.value,juz.value,h.total,h.akhir];
+  let data=JSON.parse(localStorage.getItem(KEY))||[];
+  data.push(d);
+  localStorage.setItem(KEY,JSON.stringify(data));
+  alert('Tersimpan');
 }
